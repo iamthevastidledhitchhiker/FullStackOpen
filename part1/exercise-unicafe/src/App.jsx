84feaa10431f13831b2ebc1efa3c51f95b92a26b
Keith Davis <iamthevastidledhitchhiker@users.noreply.github.com>
2024-06-01 09:06:39 +0300

    add completed exercise 1.7

--- part1/exercise-unicafe/src/App.jsx
+++ part1/exercise-unicafe/src/App.jsx
@@ -27,8 +27,11 @@ function App() {
           <div className="feedbackstatistics">
           <h1>Statistics</h1>
           <p>Good: {good}</p>
-            <p>Neutral: {neutral}</p>
-            <p>Bad: {bad}</p>
+          <p>Neutral: {neutral}</p>
+          <p>Bad: {bad}</p>
+          <p>All: {good + neutral + bad}</p>
+          <p>Average: {(good - bad) / (good + bad)}</p>
+          <p>Positive: {(good / (good + neutral + bad)) * 100}%</p>
           </div>
       </>
   )
