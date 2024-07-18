c2a41459d24364116483b108de6c793b7ee94667
Keith Davis <iamthevastidledhitchhiker@users.noreply.github.com>
2024-07-18 13:05:40 +0300

    part2 courseinfo exercises mistakenly included in part1 moved to part2

---
+++ part2/exercise-courseinfo/src/Course.jsx
@@ -0,0 +1,42 @@
+﻿//Header component. Render the name of the course.
+const  Header = ({course}) => {
+    return(
+        <h1>{course.name}</h1>
+    )
+}
+
+const Part = ({part}) => {
+    return (
+        <p>{part.name} {part.exercises}</p>
+    )
+}
+
+
+//Content component. Render part name and number of exercises.
+const Content = ({parts}) => {
+    return (
+        <div>
+            {parts.map(part => <Part key={part.name} part={part} />)}
+        </div>
+    )
+}
+
+//Total component. Render the total number of exercises.
+const Total = ({parts}) => {
+    const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+    return(
+        <p>Number of exercises {total}</p>
+    )
+}
+
+const Course = ({course}) => {
+    return (
+        <div>
+            <Header course={course} />
+            <Content parts={course.parts}/>
+            <Total parts={course.parts}/>
+        </div>
+    )
+}
+
+export default Course;
