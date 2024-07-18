bbc412fb3acc85aa62e5e6f968e4a7792671a7c7
Keith Davis <iamthevastidledhitchhiker@users.noreply.github.com>
2024-07-18 15:11:16 +0300

    add separate component that renders a single person's detail

--- part2/exercise-phonebook/src/App.jsx
+++ part2/exercise-phonebook/src/App.jsx
@@ -1,12 +1,5 @@
 import { useState } from 'react'
-
-const Person = ({ person }) => {
-    return (
-        <>
-        <li>{person.name} {person.number}</li>
-        </>
-    )
-}
+import Person from './components/Person'
 
 const App = () => {
     const [persons, setPersons] = useState([
---
+++ part2/exercise-phonebook/src/components/Person.jsx
@@ -0,0 +1,9 @@
+﻿const Person = ({ person }) => {
+    return (
+        <>
+            <li>{person.name} {person.number}</li>
+        </>
+    )
+}
+
+export default Person
