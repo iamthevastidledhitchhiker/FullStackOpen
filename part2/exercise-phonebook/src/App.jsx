419616f4cc2f4725ab511913715fc23bc547b100
Keith Davis <iamthevastidledhitchhiker@users.noreply.github.com>
2024-07-18 13:25:20 +0300

    add solution for 2.6

--- part2/exercise-phonebook/src/App.jsx
+++ part2/exercise-phonebook/src/App.jsx
@@ -1,35 +1,53 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Person = ({ person }) => {
+    return (
+        <li>{person.name}</li>
+    )
+}
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const App = () => {
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas' }
+    ])
+    const [newName, setNewName] = useState('')
+    
+    const handleNewName= (event) => {
+        console.log(event.target.value)
+        setNewName(event.target.value)
+    }
+    
+    const addPerson = (event) => {
+        event.preventDefault()
+        console.log('button clicked', event.target)
+        const personObject = {
+            name: newName
+        }
+        setPersons(persons.concat(personObject))
+        setNewName('')
+    }
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={addPerson}>
+                <div>
+                    name: <input
+                    value={newName}
+                    onChange={handleNewName}
+                />
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            <ul>
+                {persons.map((person, index) => <Person key={index} person={person}/>)}
+            </ul>
+
+        </div>
+    )
 }
 
 export default App
